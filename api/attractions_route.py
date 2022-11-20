7b0fad246c30a29a6ea453ecb83e01bfb597920f
Logan Liao <109937255+Logan645@users.noreply.github.com>
2022-11-20 21:54:49 +0800

    fix additional quotes

--- api/attractions_route.py
+++ api/attractions_route.py
@@ -22,16 +22,16 @@ def api_attractions():
         for i in data:
             images=json.loads(i[9])
             item = {
-                '"id"': i[0],
-                '"name"': i[1],
-                '"category"': i[2],
-                '"description"': i[3],
-                '"address"': i[4],
-                '"transport"': i[5],
-                '"mrt"': i[6],
-                '"lat"': i[7],
-                '"lng"': i[8],
-                '"images"': images
+                "id": i[0],
+                "name": i[1],
+                "category": i[2],
+                "description": i[3],
+                "address": i[4],
+                "transport": i[5],
+                "mrt": i[6],
+                "lat": i[7],
+                "lng": i[8],
+                "images": images
             }
             attractions_arr.append(item)
         skip_row = (page+1)*12
@@ -45,15 +45,15 @@ def api_attractions():
             next_page = None
         else:
             next_page = page + 1
-        return {
-            '"nextPage"':next_page,
-            '"data"':attractions_arr
-        }
+        return jsonify({
+            "nextPage":next_page,
+            "data":attractions_arr
+        })
     except:
-        return {
-        '"error"': True,
-        '"message"': "發生錯誤"
-        },500
+        return jsonify({
+        "error": True,
+        "message": "發生錯誤"
+        }),500
 
 @attractions.route('/api/attraction/<attractionId>')
 def api_attractionId(attractionId):
@@ -61,46 +61,34 @@ def api_attractionId(attractionId):
     # data = Attraction.query.filter_by(id=attractionId).first()
     sql = 'select * from attractions where id = {}'.format(attractionId)
     data = db.engine.execute(sql).fetchone()
-    # result = {
-    #         "id": data[0],
-    #         "name": data[1],
-    #         "category": data[2],
-    #         "description": data[3],
-    #         "address": data[4],
-    #         "transport": data[5],
-    #         "mrt": data[6],
-    #         "lat": data[7],
-    #         "lng": data[8],
-    #         "images": images
-    #         }
     try:
         images=json.loads(data[9])
-        return {
-            '"data"' : {
-            '"id"': data[0],
-            '"name"': data[1],
-            '"category"': data[2],
-            '"description"': data[3],
-            '"address"': data[4],
-            '"transport"': data[5],
-            '"mrt"': data[6],
-            '"lat"': data[7],
-            '"lng"': data[8],
-            '"images"': images
+        return jsonify ({
+            "data" : {
+            "id": data[0],
+            "name": data[1],
+            "category": data[2],
+            "description": data[3],
+            "address": data[4],
+            "transport": data[5],
+            "mrt": data[6],
+            "lat": data[7],
+            "lng": data[8],
+            "images": images
             }
-        }
+        })
     except:
-        return {
-        '"error"': True,
-        '"message"': "發生錯誤，ID搜尋不到"
-        },500
+        return jsonify({
+        "error": True,
+        "message": "發生錯誤，ID搜尋不到"
+        }),500
 
 @attractions.route('/api/categories')
 def show_categories():
     sql = 'select category from attractions'
     data = db.engine.execute(sql)
-    print(data)
-    print(type(data))
+    # print(data)
+    # print(type(data))
     try:
         category_list = []
         for i in data:
@@ -111,21 +99,31 @@ def show_categories():
                 pass
             else:  
                 category_list.append(category)
-        print(category_list)
-        return {
-            '"data"':category_list
-        }
+        # print(category_list)
+        return jsonify({
+            "data":category_list
+        })
     except:
-        return {
-        '"error"': True,
-        '"message"': "發生錯誤"
-        },500
+        return jsonify({
+        "error": True,
+        "message": "發生錯誤"
+        }), 500
 
-# @attractions.route('/api/test')
+# @attractions.route('/test')
 # def test_sql():
 #     sql_cmd = "select *from attractions where id = 1"
 #     query_data = db.engine.execute(sql_cmd).fetchall()
 #     print(query_data)
+#     result = {
+# 		"name" : query_data[0][1]
+# 		}
+#     #底下兩個寫法引號都會消失 
+#     #瀏覽器可能有安裝一些 JSON 格式的閱覽工具，會幫你整理成好閱讀的形式，如果你仔細看原始的回應資訊就不是那麼回事
 #     return jsonify({
-# 		'name':query_data[0][1]
+# 		"name" : query_data[0][1]
 # 		})
+#     return json.dumps({
+# 		"name" : query_data[0][1]
+# 		})    
+#     return jsonify(result) 
+#     return json.dumps(result)
